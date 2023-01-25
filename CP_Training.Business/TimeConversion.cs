35d845ae658b8c483b2775bb2ca22ca322b121b4
Talles Crhistian Arriel <talles.arriel@hvex.com.br>
2023-01-25 10:08:00 -0300

    Feature: Adicionando Time Converion

---
+++ CP_Training.Business/TimeConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace CP_Training.Business
+{
+    public class TimeConversion
+    {
+        public string timeConversion(string s)
+        {
+            string timeConversion24 = "";
+            DateTime dataOut;
+
+            if (DateTime.TryParseExact(s, "hh:mm:sstt", CultureInfo.InvariantCulture, DateTimeStyles.None, out dataOut))
+            {
+                timeConversion24 = dataOut.ToString("HH:mm:ss");
+            }
+
+            return timeConversion24;
+        }
+    }
+}
---
+++ CP_Training.UnitTests/TimeConversionTest.cs
@@ -0,0 +1,26 @@
+﻿using CP_Training.Business;
+using NUnit.Framework;
+using System;
+
+namespace CP_Training.UnitTests
+{
+    public class TimeConversionTest
+    {
+        private TimeConversion _timeConversion;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _timeConversion = new TimeConversion();
+        }
+
+        [TestCase("07:05:45 AM", "07:05:45")]
+        [TestCase("07:05:45 PM", "19:05:45")]
+        public void TimeConversion_Conversion12For24_ReturnConversion24(string dateTime, string expectedResult)
+        {
+            string result = _timeConversion.timeConversion(dateTime);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}

a61589d8be7e35aaea4d2c1db6b85d7287f233b7
Talles Crhistian Arriel <talles.arriel@hvex.com.br>
2023-01-30 18:34:39 -0300

    feature: Adicionando LonelyInterger

---
+++ CP_Training.Business/LonelyInterger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CP_Training.Business
+{
+    public class LonelyInterger
+    {
+        public int lonelyinteger(List<int> a)
+        {
+            List<int> valuesKeys = new List<int>();
+            int result = new int();
+            a.Sort();
+
+            Dictionary<int, int> numberCounts = new Dictionary<int, int>();
+
+            foreach (int number in a)
+            {
+                if (numberCounts.ContainsKey(number))
+                {
+                    numberCounts[number]++;
+                }
+                else
+                {
+                    numberCounts[number] = 1;
+                }
+            }
+
+            int i = 0;
+            foreach (int value in numberCounts.Values)
+            {
+                if (value == 1)
+                {
+                    foreach (int key in numberCounts.Keys)
+                    {
+                        valuesKeys = numberCounts.Keys.ToList();
+                        result = valuesKeys[i];
+                    }
+                }
+                i++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CP_Training.UnitTests/LonelyIntergerTest.cs
@@ -0,0 +1,30 @@
+﻿using CP_Training.Business;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CP_Training.UnitTests
+{
+    public class LonelyIntergerTest
+    {
+        private LonelyInterger _lonelyInterger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _lonelyInterger = new LonelyInterger();
+        }
+
+        [Test]
+        public void lonelyInteger_ElementThatOccursOnlyOnce()
+        {
+            List<int> a = new List<int>() { 59, 88, 14, 8, 85, 1, 94, 74, 57, 96, 39, 2, 47, 43, 35, 17, 53, 52, 92, 31, 99, 48, 94,
+                30, 92, 60, 32, 45, 88, 13, 39, 50, 22, 65, 89, 46, 65, 76, 57, 67, 99, 35, 76, 46, 85, 82, 45, 62, 53, 80, 74, 22, 31, 52,
+                82, 13, 41, 96, 2, 1, 80, 62, 4, 20, 50, 89, 59, 67, 60, 8, 41, 14, 47, 48, 17, 4, 43, 30, 32 };
+            int expectedResult = 20;
+
+            int result = _lonelyInterger.lonelyinteger(a);
+
+            Assert.That(result == expectedResult);
+        }
+    }
+}

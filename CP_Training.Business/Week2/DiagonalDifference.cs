c1c42a97f7b6d259e9aa20d3ba0a5119b774215d
Talles Crhistian Arriel <talles.arriel@hvex.com.br>
2023-02-01 21:13:52 -0300

    Feature: Adicionando DiagonalDifference

---
+++ CP_Training.Business/Week2/DiagonalDifference.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CP_Training.Business.Week2
+{
+    public class DiagonalDifference
+    {
+        public int diagonalDifference(List<List<int>> arr)
+        {
+            List<int> diagonalOne = new List<int>();
+            List<int> diagonalTwo = new List<int>();
+            int indiceRevert = 0;
+            int sunOne = 0;
+            int sunTwo = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                diagonalOne.Add(arr[i][i]);
+            }
+
+            for (int i = arr.Count - 1; i >= 0; i--)
+            {
+                diagonalTwo.Add(arr[i][indiceRevert]);
+                indiceRevert++;
+            }
+
+            foreach (int number in diagonalOne)
+            {
+                sunOne += number;
+            }
+
+            foreach (int number in diagonalTwo)
+            {
+                sunTwo += number;
+            }
+
+            int result = Math.Abs(sunOne - sunTwo);
+
+            return result;
+        }
+    }
+}
---
+++ CP_Training.UnitTests/Week2Tests/DiagonalDifferenceTest.cs
@@ -0,0 +1,31 @@
+﻿using CP_Training.Business.Week2;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CP_Training.UnitTests.Week2Tests
+{
+    public class DiagonalDifferenceTest
+    {
+        DiagonalDifference _diagonalDifference;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _diagonalDifference = new DiagonalDifference();
+        }
+
+        [Test]
+        public void DiagonalDifference_DifferenceOfDiagonalSums_AbsoluteDifferenceDiagonals()
+        {
+            List<int> valuesRow1 = new List<int>() { 11, 2, 4 };
+            List<int> valuesRow2 = new List<int>() { 4, 5, 6 };
+            List<int> valuesRow3 = new List<int>() { 10, 8, -12 };
+            List<List<int>> arr = new List<List<int>>() { valuesRow1, valuesRow2, valuesRow3 };
+            int expectedResult = 15;
+
+            int result = _diagonalDifference.diagonalDifference(arr);
+
+            Assert.That(result == expectedResult);
+        }
+    }
+}

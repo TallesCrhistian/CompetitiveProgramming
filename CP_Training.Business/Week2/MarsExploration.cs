3f4585ec1f6580e236c01495b65e42effe7c0099
Talles Crhistian Arriel <talles.arriel@hvex.com.br>
2023-02-05 20:28:39 -0300

    Feature: Adicionando MarsExploration

---
+++ CP_Training.Business/Week2/MarsExploration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CP_Training.Business.Week2
+{
+    public class MarsExploration
+    {
+        public int marsExploration(string s)
+        {
+            int chunkCount = (int)Math.Ceiling((double)s.Length / 3);
+            string[] result = new string[chunkCount];
+            string baseString = "SOS";
+            int resultInt = 0;
+
+            for (int i = 0; i < s.Length; i += 3)
+            {
+                result[i / 3] = new string(s.Skip(i).Take(3).ToArray());
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int e = 0; e < baseString.Length; e++)
+                {
+                    if (result[i][e] != baseString[e])
+                    {
+                        resultInt++;
+                    }
+                }
+            }
+
+            return resultInt;
+        }
+    }
+}
+
---
+++ CP_Training.UnitTests/Week2Tests/MarsExplorationTest.cs
@@ -0,0 +1,25 @@
+﻿using CP_Training.Business.Week2;
+using NUnit.Framework;
+
+namespace CP_Training.UnitTests.Week2Tests
+{
+    public class MarsExplorationTest
+    {
+       private MarsExploration _marsExploration;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _marsExploration = new MarsExploration();
+        }
+
+        [TestCase("SOSSPSSQSSOR", 3)]
+        public void MarsExploration_RepairMessage(string s, int expectedResult)
+        {
+            int result = _marsExploration.marsExploration(s);
+
+            Assert.That(result == expectedResult);
+        }
+
+    }
+}

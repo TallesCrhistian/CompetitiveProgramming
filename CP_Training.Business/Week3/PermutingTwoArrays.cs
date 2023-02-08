c9d9f063b27cdc9da72e3f0154f311f7a9b065ef
Talles Crhistian Arriel <talles.arriel@hvex.com.br>
2023-02-07 21:13:16 -0300

    Feature: Adicionando PermutingTwoArrays

---
+++ CP_Training.Business/Week3/PermutingTwoArrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CP_Training.Business.Week3
+{
+    public class PermutingTwoArrays
+    {
+        public string twoArrays(int k, List<int> A, List<int> B)
+        {           
+            A.Sort();
+            B.Sort();
+            int n = A.Count;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (A[i] + B[n - i - 1] < k)
+                {
+                    return "NO";
+                }
+            }
+
+            return "YES";
+        }
+    }
+}
---
+++ CP_Training.UnitTests/Week3Tests/PermutingTwoArraysTest.cs
@@ -0,0 +1,42 @@
+﻿using CP_Training.Business.Week3;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CP_Training.UnitTests.Week3Tests
+{
+    public class PermutingTwoArraysTest
+    {
+        private PermutingTwoArrays _permutingTwoArrays;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _permutingTwoArrays = new PermutingTwoArrays();
+        }
+
+        [Test]
+        public void TwoArrays_PermutationTwoArrays_ReturnYES()
+        {
+            List<int> A = new List<int> { 2, 1, 3 };
+            List<int> B = new List<int> { 7, 8, 9 };
+            int k = 10;
+            string expectedResult = "YES";
+
+            string result = _permutingTwoArrays.twoArrays(k, A, B);
+
+            Assert.That(result == expectedResult);
+        }
+
+        public void TwoArrays_PermutationTwoArrays_ReturnNO()
+        {
+            List<int> A = new List<int> { 1, 2, 2, 1 };
+            List<int> B = new List<int> { 3, 3, 3, 4 };
+            int k = 5;
+            string expectedResult = "NO";
+
+            string result = _permutingTwoArrays.twoArrays(k, A, B);
+
+            Assert.That(result == expectedResult);
+        }
+    }
+}
